7faf112bd4b87cad4e7eb13e8e6f2f3fef069c46
Matusbh <matusbehun03@gmail.com>
2026-09-01 07:35:08 +0100

    Cambios en users routes y test en login

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -48,24 +48,33 @@ router.post("/signup", async (req, res) => {
 
 // POST api login
 router.post("/login", async (req, res) => {
-  user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Email or password invalid");
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Email or password invalid");
 
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send("Email or password invalid");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password,
+    );
+    if (!validPassword)
+      return res.status(400).send("Email or password invalid");
 
-  const token = jwt.sign(
-    {
-      _id: user._id,
-      role: user.role,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIN: "1h",
-    },
-  );
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      },
+    );
 
-  res.header("Authorization", token).send(token);
+    res.status(200).header("Authorization", token).json({ token: token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
 });
 
 export default router;
